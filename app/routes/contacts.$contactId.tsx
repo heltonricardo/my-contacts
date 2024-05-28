1dbd95da8151910780af74782d0d66da35c17b16
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2024-05-28 10:26:32 -0300

    feat: loading contact

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,17 +1,22 @@
-import { Form } from "@remix-run/react";
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
 import type { FunctionComponent } from "react";
+import invariant from "tiny-invariant";
+import type { ContactRecord } from "~/data";
+import { getContact } from "~/data";
 
-import type { ContactRecord } from "../data";
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  invariant(params.contactId, "Missing contactId param");
+  const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response("Not Found", { status: 404 });
+  }
+  return json({ contact });
+};
 
 export default function Contact() {
-  const contact = {
-    first: "Your",
-    last: "Name",
-    avatar: "https://placedog.net/200/200/g",
-    twitter: "your_handle",
-    notes: "Some notes",
-    favorite: true,
-  };
+  const { contact } = useLoaderData<typeof loader>();
 
   return (
     <div id="contact">
@@ -37,7 +42,9 @@ export default function Contact() {
 
         {contact.twitter ? (
           <p>
-            <a href={`https://twitter.com/${contact.twitter}`}>{contact.twitter}</a>
+            <a href={`https://twitter.com/${contact.twitter}`} target="_blank" rel="noreferrer">
+              {contact.twitter}
+            </a>
           </p>
         ) : null}
 
