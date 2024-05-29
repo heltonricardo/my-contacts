bb48d27a37dbefe02a5bb4a34ba57a340acabe09
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2024-05-29 19:00:46 -0300

    feat: index page

---
+++ app/routes/_index.tsx
@@ -0,0 +1,14 @@
+export default function Index() {
+  return (
+    <div id="index">
+      <p id="index-page">
+        My Contacts app was made by Helton Ricardo.
+        <br />
+        Built using{" "}
+        <a href="https://remix.run" target="_blank" rel="noreferrer">
+          remix.run
+        </a>
+      </p>
+    </div>
+  );
+}
